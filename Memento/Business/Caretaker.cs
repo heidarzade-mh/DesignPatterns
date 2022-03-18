534dec89e24f8a8bfc420fd8b630470b6b5c3ecf
Mohammad Hossein Heydarzadeh <h.heydarzadeh@gmail.com>
2022-03-18 15:04:22 +0330

    add Memento design pattern

---
+++ Memento/Business/Caretaker.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Memento.Business
+{
+    public class Caretaker
+    {
+        private List<Models.Memento> ledTvList = new List<Models.Memento>();
+
+        public void AddMemento(Models.Memento m)
+        {
+            ledTvList.Add(m);
+            Console.WriteLine("LED TV's snapshots Maintained by CareTaker :" + m.GetDetails());
+        }
+
+        public Models.Memento GetMemento(int index)
+        {
+            return ledTvList[index];
+        }
+    }
+}
---
+++ Memento/Business/Originator.cs
@@ -0,0 +1,24 @@
+﻿using Memento.Models;
+
+namespace Memento.Business
+{
+    public class Originator
+    {
+        public LEDTV ledTV;
+
+        public Models.Memento CreateMemento()
+        {
+            return new Models.Memento(ledTV);
+        }
+
+        public void SetMemento(Models.Memento memento)
+        {
+            ledTV = memento.ledTV;
+        }
+
+        public string GetDetails()
+        {
+            return "Originator [ledTV=" + ledTV.GetDetails() + "]";
+        }
+    }
+}
