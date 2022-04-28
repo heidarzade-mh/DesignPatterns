b05a6887d5aa63f5b6bc225888003b0478baafbc
Mohammad Hossein Heydarzadeh <h.heydarzadeh@gmail.com>
2022-04-29 01:28:45 +0430

    add chain of responsibility

---
+++ ChainOfResponsibility/Handler.cs
@@ -0,0 +1,13 @@
+﻿namespace ChainOfResponsibility
+{
+    public abstract class Handler
+    {
+        public Handler rsHandler;
+        public void nextHandler(Handler rsHandler)
+        {
+            this.rsHandler = rsHandler;
+        }
+
+        public abstract void dispatchRs(long requestedAmount);
+    }
+}
