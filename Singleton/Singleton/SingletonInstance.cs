ca47492c676ed7b697b0579dc6b797693ae90c04
Mohammad Hossein Heydarzadeh <h.heydarzadeh@gmail.com>
2022-04-28 16:57:06 +0430

    add Singleton design pattern

---
+++ Singleton/Singleton/SingletonInstance.cs
@@ -0,0 +1,18 @@
+﻿namespace Singleton
+{
+    public class SingletonInstance
+    {
+        private SingletonInstance _instance;
+        private SingletonInstance() {}
+
+        public SingletonInstance GetInstance()
+        {
+            if (_instance == null)
+            {
+                _instance = new SingletonInstance();
+            }
+
+            return _instance;
+        }
+    }
+}
